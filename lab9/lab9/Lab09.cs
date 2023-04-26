73c78c5251a5244cb8afe4fa00690dc623deab23
SaintAngeLs <96334335+SaintAngeLs@users.noreply.github.com>
2023-04-26 23:53:15 +0200

    Add files via upload

---
+++ lab9/lab9/Lab09.cs
@@ -0,0 +1,190 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ASD.Graphs;
+
+/// <summary>
+/// Klasa rozszerzająca klasę Graph o rozwiązania problemów największej kliki i izomorfizmu grafów metodą pełnego przeglądu (backtracking)
+/// </summary>
+public static class Lab10GraphExtender
+{
+    /// <summary>
+    /// Wyznacza największą klikę w grafie i jej rozmiar metodą pełnego przeglądu (backtracking)
+    /// </summary>
+    /// <param name="g">Badany graf</param>
+    /// <param name="clique">Wierzchołki znalezionej największej kliki - parametr wyjściowy</param>
+    /// <returns>Rozmiar największej kliki</returns>
+    /// <remarks>
+    /// Nie wolno modyfikować badanego grafu.
+    /// </remarks>
+    /// 
+    public static int MaxClique(this Graph g, out int[] clique)
+    {
+        int[] currentClique = new int[g.VertexCount];
+
+        int[] maxClique = new int[g.VertexCount];
+
+        int maxSize = 0;
+
+        MaxCliqueRec(g, 0, currentClique, 0, ref maxClique, ref maxSize);
+
+        clique = new int[maxSize];
+
+        Array.Copy(maxClique, clique, maxSize);
+
+        return maxSize;
+    }
+
+    private static void MaxCliqueRec(Graph g, int vertex, int[] currentClique, int currentSize, ref int[] maxClique, ref int maxSize)
+    {
+        if (vertex == g.VertexCount)
+        {
+            if (currentSize > maxSize)
+            {
+
+                maxSize = currentSize;
+
+                maxClique = (int[])currentClique.Clone();
+
+            }
+            return;
+        }
+
+        bool canAdd = true;
+
+        foreach (int v in currentClique.Take(currentSize))
+        {
+            if (!g.HasEdge(vertex, v))
+            {
+
+                canAdd = false;
+
+                break;
+
+            }
+        }
+
+        if (canAdd)
+        {
+
+            currentClique[currentSize] = vertex;
+
+            MaxCliqueRec(g, vertex + 1, currentClique, currentSize + 1, ref maxClique, ref maxSize);
+
+        }
+
+        MaxCliqueRec(g, vertex + 1, currentClique, currentSize, ref maxClique, ref maxSize);
+    }
+
+    /// <summary>
+    /// Bada izomorfizm grafów metodą pełnego przeglądu (backtracking)
+    /// </summary>
+    /// <param name="g">Pierwszy badany graf</param>
+    /// <param name="h">Drugi badany graf</param>
+    /// <param name="map">Mapowanie wierzchołków grafu h na wierzchołki grafu g (jeśli grafy nie są izomorficzne to null) - parametr wyjściowy</param>
+    /// <returns>Informacja, czy grafy g i h są izomorficzne</returns>
+    /// <remarks>
+    /// 1) Uwzględniamy wagi krawędzi
+    /// 3) Nie wolno modyfikować badanych grafów.
+    /// </remarks>
+    /// 
+    public static bool IsomorphismTest(this Graph<int> g, Graph<int> h, out int[] map)
+    {
+        map = null;
+
+        // przypadek gdy g ma mniej wierzchołków niżgraf h: nie mogą byś te grafy izomorficzne
+
+        if (g.VertexCount < h.VertexCount)
+
+            return false;
+
+        // wierzchołki zuzyte
+
+        var used = new bool[g.VertexCount];
+
+        int vc = 0;
+
+        map = new int[g.VertexCount];
+
+
+        // Jeśli znaleziono mapowanie, zwróć true, w przeciwnym razie zwróć false i ustaw mapowanie na null
+        if (FindMapping(g, h, used, vc, ref map))
+
+            return true;
+
+        map = null;
+
+        return false;
+    }
+    /// <summary>
+    /// Rekurencyjna metoda, która próbuje znaleźć mapowanie wierzchołków grafów g i h
+    /// </summary>
+    /// <param name="g">Pierwszy badany graf</param>
+    /// <param name="h">Drugi badany graf</param>
+    /// <param name="used">Tablica oznaczająca, które wierzchołki grafu g zostały już użyte</param>
+    /// <param name="vc">Liczba aktualnie mapowanych wierzchołków</param>
+    /// <param name="iso">Referencja do tablicy mapowania</param>
+    /// <returns>Informacja, czy znaleziono poprawne mapowanie wierzchołków grafów g i h</returns>
+    /// 
+    public static bool FindMapping(Graph<int> g, Graph<int> h, bool[] used, int vc, ref int[] iso)
+    {
+        // Jeśli liczba mapowanych wierzchołków równa się liczbie wierzchołków w grafie g, zwróć true
+
+
+        if (vc == g.VertexCount)
+            return true;
+
+        // Przeszukaj wszystkie wierzchołki grafu g
+
+        for (int i = 0; i < g.VertexCount; i++)
+        {
+
+            int j = 0;
+
+            // Jeśli wierzchołek nie został użyty i liczba sąsiadów w grafie g i h jest równa, sprawdź mapowaniedla tego wierzchołka
+
+            if (!used[i] && g.OutNeighbors(i).Count() == h.OutNeighbors(vc).Count())
+            {
+                // Sprawdź, czy krawędzie między zmapowanymi wierzchołkami są izomorficzne
+
+                for (j = 0; j < vc; j++)
+                {
+                    // Jeśli krawędzie nie są izomorficzne, przerwij pętlę
+
+                    if (g.HasEdge(iso[j], i) != h.HasEdge(j, vc) || g.HasEdge(iso[j], i) && g.GetEdgeWeight(iso[j], i) != h.GetEdgeWeight(j, vc))
+                        break;
+                }
+
+                // Jeśli krawędzie izomorficzne nie są, kontynuuj przeszukiwanie wierzchołków
+
+                if (j < vc)
+                    continue;
+
+                // Jeśli krawędzie są izomorficzne, zaktualizuj mapowanie i tablicę użytych wierzchołków
+
+                iso[vc] = i;
+
+                used[i] = true;
+
+                // Wywołaj rekurencyjnie metodę FindMapping dla kolejnego wierzchołka
+
+                if (FindMapping(g, h, used, vc + 1, ref iso))
+                    return true;
+
+                // Jeśli rekurencyjne wywołanie nie znalazło mapowania, cofnij zmiany w mapowaniu i tablicy użytych wierzchołków
+
+                used[i] = false; 
+            }   
+
+        }
+
+        // Jeśli nie znaleziono mapowania dla żadnego wierzchołka, zwróć false
+
+        return false;
+        
+    }
+
+}
+
---
+++ lab9_election/lab9_election/lab9_election/Lab09.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ASD.Graphs;
+
+namespace ASD
+{
+    public class Lab08 : MarshalByRefObject
+    {
+        /// <summary>
+        /// Znajduje cykl rozpoczynający się w stolicy, który dla wybranych miast,
+        /// przez które przechodzi ma największą sumę liczby ludności w tych wybranych
+        /// miastach oraz minimalny koszt.
+        /// </summary>
+        /// <param name="cities">
+        /// Graf miast i połączeń między nimi.
+        /// Waga krawędzi jest kosztem przejechania między dwoma miastami.
+        /// Koszty transportu między miastami są nieujemne.
+        /// </param>
+        /// <param name="citiesPopulation">Liczba ludności miast</param>
+        /// <param name="meetingCosts">
+        /// Koszt spotkania w każdym z miast.
+        /// Dla części pierwszej koszt spotkania dla każdego miasta wynosi 0.
+        /// Dla części drugiej koszty są nieujemne.
+        /// </param>
+        /// <param name="budget">Budżet do wykorzystania przez kandydata.</param>
+        /// <param name="capitalCity">Numer miasta będącego stolicą, z której startuje kandydat.</param>
+        /// <param name="path">
+        /// Tablica dwuelementowych krotek opisująca ciąg miast, które powinen odwiedzić kandydat.
+        /// Pierwszy element krotki to numer miasta do odwiedzenia, a drugi element decyduje czy
+        /// w danym mieście będzie organizowane spotkanie wyborcze.
+        /// 
+        /// Pierwszym miastem na tej liście zawsze będzie stolica (w której można, ale nie trzeba
+        /// organizować spotkania).
+        /// 
+        /// Zakładamy, że po odwiedzeniu ostatniego miasta na liście kandydat wraca do stolicy
+        /// (na co musi mu starczyć budżetu i połączenie między tymi miastami musi istnieć).
+        /// 
+        /// Jeżeli kandydat nie wyjeżdża ze stolicy (stolica jest jedynym miastem, które odwiedzi),
+        /// to lista `path` powinna zawierać jedynie jeden element: stolicę (wraz z informacją
+        /// czy będzie tam spotkanie czy nie). Nie są wtedy ponoszone żadne koszty podróży.
+        /// 
+        /// W pierwszym etapie drugi element krotki powinien być zawsze równy `true`.
+        /// </param>
+        /// <returns>
+        /// Liczba mieszkańców, z którymi spotka się kandydat.
+        /// </returns>
+        public int ComputeElectionCampaignPath(Graph<int> cities, int[] citiesPopulation,
+            double[] meetingCosts, double budget, int capitalCity, out (int, bool)[] path)
+        {
+            path = new (int, bool)[1];
+            path[0] = (capitalCity, false);
+            int maxCount = -1;
+            double maxCost = 0;
+
+            List<int> cycle = new List<int>();
+
+            List<int> maxCycle = null;
+
+            double cost = 0;
+
+            int count = 0;
+
+            bool[] visited = new bool[cities.VertexCount];
+
+            bool[] org = new bool[cities.VertexCount];
+
+            bool[] maxOrg = new bool[cities.VertexCount];
+
+            cycle.Add(capitalCity);
+
+            visited[capitalCity] = true;
+
+            if (meetingCosts[capitalCity] != 0)
+                GenerateCycle();
+
+            org[capitalCity] = true;
+
+            cost = meetingCosts[capitalCity];
+
+            count = citiesPopulation[capitalCity];
+
+            GenerateCycle();
+
+            if (maxCycle != null)
+            {
+
+                path = new (int, bool)[maxCycle.Count];
+
+                for (int i = 0; i < maxCycle.Count; i++)
+                    path[i] = (maxCycle[i], maxOrg[maxCycle[i]]);
+
+            }
+            return maxCount;
+
+            void GenerateCycle()
+            {
+                var next = cities.OutNeighbors(cycle.Last());
+
+                if (cost > budget)
+                { 
+                    return;
+                }
+
+                if ((count > maxCount || (count == maxCount && cost < maxCost)) && (cycle.Last() == capitalCity ||
+                    (cities.HasEdge(cycle.Last(), capitalCity) && cost + cities.GetEdgeWeight(cycle.Last(), capitalCity) <= budget)))
+                {
+
+                    maxCost = cost;
+
+                    maxCount = count;
+
+                    maxCycle = cycle.ToList();
+
+                    maxOrg = org.ToArray();
+
+                }
+
+                foreach (var v in next)
+                {
+
+                    if (!visited[v])
+                    {
+
+                        cost += cities.GetEdgeWeight(cycle.Last(), v);
+
+                        cycle.Add(v);
+
+                        visited[v] = true;
+
+                        if (meetingCosts[v] != 0)
+                            GenerateCycle();
+
+                        org[v] = true;
+
+                        cost += meetingCosts[v];
+
+                        count += citiesPopulation[v];
+
+                        GenerateCycle();
+
+                        cycle.Remove(v);
+
+                        cost -= cities.GetEdgeWeight(cycle.Last(), v);
+
+                        cost -= meetingCosts[v];
+
+                        count -= citiesPopulation[v];
+
+                        visited[v] = false;
+
+                        org[v] = false;
+
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
